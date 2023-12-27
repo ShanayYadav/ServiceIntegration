957218ccca0c0b6aa0971482262928dbe8cb2f64
shanayyadav <pfloxtechpvt@gmail.com>
2023-12-27 08:49:26 +0530

    RestSharp wrapper

--- GoogleIntegration/Imp/GoogleAuthApi.cs
+++ GoogleIntegration/Imp/GoogleAuthApi.cs
@@ -1,5 +1,7 @@
 ﻿using GoogleIntegration.Abstraction;
 using GoogleIntegration.Domain.Auth;
+using RestSharp;
+using RestShrapWrapper;
 using System.Web;
 
 namespace GoogleIntegration.Imp
@@ -10,7 +12,7 @@ namespace GoogleIntegration.Imp
 		const string GOOGLE_AUTH_URL = "https://oauth2.googleapis.com/token";
 		public string OpenConnectUrl(OpenConnectRequestModel googleAuthRequest)
 		{
-			var url = $"{GOOGLE_OPEN_Connect_AUTH_URL}?{GetParam(googleAuthRequest)}";
+			var url = $"{GOOGLE_OPEN_Connect_AUTH_URL}?{RestSharpUrlUtility.GetParam(googleAuthRequest)}";
 			return url;
 		}
 
@@ -26,24 +28,14 @@ namespace GoogleIntegration.Imp
 
 		public AuthResponseModel GenerateAuthToken(AuthRequestModel authRequestModel)
 		{
-			return new AuthResponseModel { };
-		}
-
-		private string GetParam<TSource>(TSource source)
-		{
-			var param = string.Empty;
-			if (source == null)
-			{
-				return param;
-			}
-			var properties = source.GetType().GetProperties()?.ToList();
-			properties?.ForEach(x =>
-			{
-				param += $"{x.Name}={x.GetValue(source)}&";
-			});
-			return param?.Substring(0, param.Length - 1);
+			var apiUrl = $"{GOOGLE_AUTH_URL}";
+			var client = new RestClient();
+			var request = new RestRequest(apiUrl, Method.Post);
+			request.AddHeader("host", "oauth2.googleapis.com");
+			request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+			var response = client.Execute<AuthResponseModel>(request);
+			return response.Data;
 		}
 
 	}
-
 }
---
+++ RestShrapWrapper/RestSharpUrlUtility.cs
@@ -0,0 +1,20 @@
+﻿namespace RestShrapWrapper
+{
+	public class RestSharpUrlUtility
+	{
+		public static string GetParam<TSource>(TSource source)
+		{
+			var param = string.Empty;
+			if (source == null)
+			{
+				return param;
+			}
+			var properties = source.GetType().GetProperties()?.ToList();
+			properties?.ForEach(x =>
+			{
+				param += $"{x.Name}={x.GetValue(source)}&";
+			});
+			return param?.Substring(0, param.Length - 1);
+		}
+	}
+}
