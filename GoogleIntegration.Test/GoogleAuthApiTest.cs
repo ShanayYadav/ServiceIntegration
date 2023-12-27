62a43285d3d76068ac8d6320466cc98604a74194
shanayyadav <pfloxtechpvt@gmail.com>
2023-12-27 12:03:47 +0530

    code refractoring

--- GoogleIntegration.Test/GoogleAuthApiTest.cs
+++ GoogleIntegration.Test/GoogleAuthApiTest.cs
@@ -50,32 +50,28 @@ namespace GoogleIntegration.Test
 		public void OpenConnectUrl_With_All_Field_Should_Return_Expected_Result()
 		{
 			_result = _googleAuthApi.OpenConnectUrl(_openConnectRequestModel);
-			Assert.IsNotNull(_result);
-			Assert.IsTrue(_result.Trim().Length > 0);
-			Assert.IsTrue(_result.Contains("Scope=https://www.googleapis.com/auth/drive.metadata.readonly", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Access_type=offline", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Include_Granted_Scopes=true", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Response_Type=code", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("State=gaurav", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Redirect_Uri=http://localhost:4200", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Prompt=consent", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Client_Id=clientid12324567890.apps.googleusercontent.com", StringComparison.InvariantCultureIgnoreCase));
+			AssertForOpenConnectUrl(_result);
 		}
 
 		[TestMethod]
 		public void OpenConnectUrl_Default_Should_Return_Expected_Result()
 		{
 			_result = _googleAuthApi.OpenConnectUrl();
-			Assert.IsNotNull(_result);
-			Assert.IsTrue(_result.Trim().Length > 0);
-			Assert.IsTrue(_result.Contains("Scope=https://www.googleapis.com/auth/drive.metadata.readonly", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Access_type=offline", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Include_Granted_Scopes=true", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Response_Type=code", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("State=gaurav", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Redirect_Uri=http://localhost:4200", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Prompt=consent", StringComparison.InvariantCultureIgnoreCase));
-			Assert.IsTrue(_result.Contains("Client_Id=clientid12324567890.apps.googleusercontent.com", StringComparison.InvariantCultureIgnoreCase));
+			AssertForOpenConnectUrl(_result);
+		}
+
+		private void AssertForOpenConnectUrl(string result)
+		{
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.Trim().Length > 0);
+			Assert.IsTrue(result.Contains("Scope=https://www.googleapis.com/auth/drive.metadata.readonly", StringComparison.InvariantCultureIgnoreCase));
+			Assert.IsTrue(result.Contains("Access_type=offline", StringComparison.InvariantCultureIgnoreCase));
+			Assert.IsTrue(result.Contains("Include_Granted_Scopes=true", StringComparison.InvariantCultureIgnoreCase));
+			Assert.IsTrue(result.Contains("Response_Type=code", StringComparison.InvariantCultureIgnoreCase));
+			Assert.IsTrue(result.Contains("State=gaurav", StringComparison.InvariantCultureIgnoreCase));
+			Assert.IsTrue(result.Contains("Redirect_Uri=http://localhost:4200", StringComparison.InvariantCultureIgnoreCase));
+			Assert.IsTrue(result.Contains("Prompt=consent", StringComparison.InvariantCultureIgnoreCase));
+			Assert.IsTrue(result.Contains("Client_Id=clientid12324567890.apps.googleusercontent.com", StringComparison.InvariantCultureIgnoreCase));
 		}
 	}
 }
