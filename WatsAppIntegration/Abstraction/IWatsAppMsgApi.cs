216b57e52d3a96e886f8db4d8f959e8ae711e284
shanayyadav <pfloxtechpvt@gmail.com>
2023-12-28 09:14:50 +0530

    rest api implementation for wats app

--- WatsAppIntegration/Abstraction/IWatsAppMsgApi.cs
+++ WatsAppIntegration/Abstraction/IWatsAppMsgApi.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WatsAppIntegration.Domain;
+﻿using WatsAppIntegration.Domain;
 
 namespace WatsAppIntegration.Abstraction
 {
--- WatsAppIntegration/Imp/WatsAppMsgApi.cs
+++ WatsAppIntegration/Imp/WatsAppMsgApi.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using RestSharp;
 using WatsAppIntegration.Abstraction;
 using WatsAppIntegration.Domain;
 using WatsAppIntegration.Domain.Config;
@@ -12,6 +8,8 @@ namespace WatsAppIntegration.Imp
 {
 	public class WatsAppMsgApi : IWatsAppMsgApi
 	{
+		const string GRAPH_MSG_API_URI = "/v17.0/{0}/messages";
+
 		FaceBookGraphApiConfig _faceBookGraphApiConfig;
 		public WatsAppMsgApi(IOptions<FaceBookGraphApiConfig> faceBookGraphApiConfig)
 		{
@@ -20,7 +18,20 @@ namespace WatsAppIntegration.Imp
 
 		public WatsAppSendMsgResponseModel Send(WatsAppSendMsgRequestModel watsSendMsgRequestModel)
 		{
-			throw new NotImplementedException();
+			var options = new RestClientOptions("")
+			{
+				MaxTimeout = -1,
+			};
+			var client = new RestClient(options);
+			var url = string.Format($"{_faceBookGraphApiConfig.GraphApiUrl}{GRAPH_MSG_API_URI}", _faceBookGraphApiConfig.WatsAppApi.PhoneNumberId);
+			watsSendMsgRequestModel.Messaging_Product = _faceBookGraphApiConfig.WatsAppApi.Messaging_Product;
+			var request = new RestRequest(url, Method.Post);
+			request.AddHeader("Authorization", _faceBookGraphApiConfig.WatsAppApi.AuthToken);
+			request.AddHeader("Content-Type", "application/json");
+			var body = "";
+			request.AddJsonBody(watsSendMsgRequestModel);
+			var response = client.Execute<WatsAppSendMsgResponseModel>(request);
+			return response.Data;
 		}
 	}
 }
